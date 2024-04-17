663efcee05c3b9334f2792d9541dc6bd6abd842e
Ayman Lyesri <lyesri99@gmail.com>
2024-04-17 10:51:38 +0100

    first commit

---
+++ argon-dashboard-angular-master/src/app/pages/maps/maps.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+declare const google: any;
+
+
+@Component({
+  selector: 'app-maps',
+  templateUrl: './maps.component.html',
+  styleUrls: ['./maps.component.scss']
+})
+export class MapsComponent implements OnInit
+{
+
+  map: Map;
+
+  constructor() { }
+
+  ngOnInit()
+  {
+
+    this.initMap();
+  }
+
+  initMap(): void
+  {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+  }
+}
